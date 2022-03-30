82ef981402df90452468cf44514fe02123b3a63b
lee-emqx <66231514+lee-emqx@users.noreply.github.com>
2022-03-30 18:52:15 +0800

    * NEW [nnc_test.py] add retain test.

--- nnc_test.py
+++ nnc_test.py
@@ -42,8 +42,32 @@ def test_clean_session():
         process.terminate()
 
 
+def test_retain():
+    retain_pub_cmd = shlex.split("mosquitto_pub -m message  -t topic -h iot-platform.cloud -p 6301 -r")
+    clean_retain_pub_cmd = shlex.split("mosquitto_pub -n  -t topic -h iot-platform.cloud -p 6301 -r")
+    sub_cmd = shlex.split("mosquitto_sub -t topic -h iot-platform.cloud -p 6301")
+    process = subprocess.Popen(retain_pub_cmd, 
+                               stdout=subprocess.PIPE,
+                               universal_newlines=True)
+
+    process = subprocess.Popen(sub_cmd, 
+                               stdout=subprocess.PIPE,
+                               universal_newlines=True)
+
+    while True:
+        output = process.stdout.readline()
+        if output.strip() == 'message':
+            process.terminate()
+            break
+
+    process = subprocess.Popen(clean_retain_pub_cmd, 
+                               stdout=subprocess.PIPE,
+                               universal_newlines=True)
+
+
 
 test_clean_session()
+test_retain()
 
 
 
