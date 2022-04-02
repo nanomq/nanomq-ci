0128d25f3f4558e66b6df546b8c45876aa12d66b
lee-emqx <66231514+lee-emqx@users.noreply.github.com>
2022-04-02 18:53:42 +0800

    * FIX [nnc_test*.py] fixed block.

--- nnc_test.py
+++ nnc_test.py
@@ -4,9 +4,17 @@ import subprocess
 import shlex
 import os
 import paho.mqtt.client as mqtt
-from multiprocessing import Process
+from multiprocessing import Process, Value
 import time
 
+def cnt_message(cmd, n, message):
+    process = subprocess.Popen(cmd,
+                               stdout=subprocess.PIPE,
+                               universal_newlines=True)
+    while True:
+        output = process.stdout.readline()
+        if output.strip() == message:
+            n.value += 1
 
 def test_clean_session():
     clean_session_cmd = shlex.split("mosquitto_sub -t topic -h localhost -p 1883")
@@ -27,16 +35,22 @@ def test_clean_session():
                                stdout=subprocess.PIPE,
                                universal_newlines=True)
 
-    process = subprocess.Popen(persist_session_cmd, 
-                               stdout=subprocess.PIPE,
-                               universal_newlines=True)
+    # process = subprocess.Popen(persist_session_cmd, 
+    #                            stdout=subprocess.PIPE,
+    #                            universal_newlines=True)
 
-    while True:
-        output = process.stdout.readline()
-        if output.strip() == 'message':
-            process.terminate()
-            break
+    # while True:
+    #     output = process.stdout.readline()
+    #     if output.strip() == 'message':
+    #         process.terminate()
+    #         break
 
+    
+    cnt = Value('i', 0)
+    process = Process(target=cnt_message, args=(persist_session_cmd, cnt, "message"))
+    process.start()
+    time.sleep(1)
+    process.terminate()
 
     process = subprocess.Popen(clean_session_cmd, 
                                stdout=subprocess.PIPE,
@@ -45,6 +59,11 @@ def test_clean_session():
         process.wait(1)
     except:
         process.terminate()
+
+    if cnt.value != 1:
+        print("Clean session test failed!")
+        return
+    
     print("Clean session test passed!")
 
 
@@ -56,20 +75,19 @@ def test_retain():
                                stdout=subprocess.PIPE,
                                universal_newlines=True)
 
-    process = subprocess.Popen(sub_cmd, 
-                               stdout=subprocess.PIPE,
-                               universal_newlines=True)
+    cnt = Value('i', 0)
+    process = Process(target=cnt_message, args=(sub_cmd, cnt, "message"))
+    process.start()
+    time.sleep(1)
 
-    while True:
-        output = process.stdout.readline()
-        if output.strip() == 'message':
-            process.terminate()
-            break
-
+    process.terminate()
     process = subprocess.Popen(clean_retain_pub_cmd, 
                                stdout=subprocess.PIPE,
                                universal_newlines=True)
-
+    process.terminate()
+    if cnt.value != 1:
+        print("Retain test failed!")
+        return
     print("Retain test passed!")
 
 def test_v4_v5():
@@ -78,35 +96,32 @@ def test_v4_v5():
     pub_cmd_v4 = shlex.split("mosquitto_pub -m message  -t topic -h localhost -p 1883 -V 311")
     pub_cmd_v5 = shlex.split("mosquitto_pub -m message  -t topic -h localhost -p 1883 -V 5")
 
-    process1 = subprocess.Popen(sub_cmd_v4,
-                               stdout=subprocess.PIPE,
-                               universal_newlines=True)
-
+    cnt = Value('i', 0)
+    process = Process(target=cnt_message, args=(sub_cmd_v4, cnt, "message"))
+    process.start()
     time.sleep(1)
     process2 = subprocess.Popen(pub_cmd_v5, 
                                stdout=subprocess.PIPE,
                                universal_newlines=True)
+    time.sleep(1)
+    process.terminate()
 
-    while True:
-        output = process1.stdout.readline()
-        if output.strip() == 'message':
-            process1.terminate()
-            break
+    if cnt.value != 1:
+        print("V4/V5 test failed!")
+        return
 
-    process3 = subprocess.Popen(sub_cmd_v5, 
-                               stdout=subprocess.PIPE,
-                               universal_newlines=True)
-
+    process = Process(target=cnt_message, args=(sub_cmd_v5, cnt, "message"))
+    process.start()
     time.sleep(1)
     process4 = subprocess.Popen(pub_cmd_v4, 
                                stdout=subprocess.PIPE,
                                universal_newlines=True)
 
-    while True:
-        output = process3.stdout.readline()
-        if output.strip() == 'message':
-            process3.terminate()
-            break
+    time.sleep(1)
+    process.terminate()
+    if cnt.value != 2:
+        print("V4/V5 test failed!")
+        return
     print("V4/V5 test passed!")
             
 
@@ -114,9 +129,9 @@ def test_will_topic():
     pub_cmd = shlex.split("mosquitto_pub -h localhost -p 1883 -t msg -d -l --will-topic will_topic --will-payload will_payload")
     sub_cmd = shlex.split("mosquitto_sub -t will_topic -h localhost -p 1883")
 
-    process1 = subprocess.Popen(sub_cmd,
-                               stdout=subprocess.PIPE,
-                               universal_newlines=True)
+    cnt = Value('i', 0)
+    process = Process(target=cnt_message, args=(sub_cmd, cnt, "will_payload"))
+    process.start()
 
     time.sleep(1)
     process2 = subprocess.Popen(pub_cmd,
@@ -125,14 +140,18 @@ def test_will_topic():
     time.sleep(1)
     process2.terminate()
     process2.kill()
+
+    times = 0
     while True:
-        output = process1.stdout.readline()
-        if output.strip() == 'will_payload':
-            process1.terminate()
+        if cnt.value == 1:
+            print("Will topic test passed!")
             break
-
-    print("Will topic test passed!")
-
+        time.sleep(1)
+        times += 1
+        if times == 5:
+            print("Will topic test failed!")
+            break
+    process.terminate()
 
 
 if __name__=='__main__':
