d6da84775ecfce613a467d2baa93b0a8b03efbbb
lee-emqx <66231514+lee-emqx@users.noreply.github.com>
2022-04-02 15:20:57 +0800

    * MDF [nnc_test*.py] modify ip/host and output.

--- nnc_test.py
+++ nnc_test.py
@@ -9,9 +9,9 @@ import time
 
 
 def test_clean_session():
-    clean_session_cmd = shlex.split("mosquitto_sub -t topic -h iot-platform.cloud -p 6301")
-    persist_session_cmd = shlex.split("mosquitto_sub -t topic -h iot-platform.cloud -p 6301 -c -i id")
-    pub_cmd = shlex.split("mosquitto_pub -m message  -t topic -h iot-platform.cloud -p 6301")
+    clean_session_cmd = shlex.split("mosquitto_sub -t topic -h localhost -p 1883")
+    persist_session_cmd = shlex.split("mosquitto_sub -t topic -h localhost -p 1883 -c -i id")
+    pub_cmd = shlex.split("mosquitto_pub -m message  -t topic -h localhost -p 1883")
 
     # persistence session
     process = subprocess.Popen(persist_session_cmd, 
@@ -45,12 +45,13 @@ def test_clean_session():
         process.wait(1)
     except:
         process.terminate()
+    print("Clean session test passed!")
 
 
 def test_retain():
-    retain_pub_cmd = shlex.split("mosquitto_pub -m message  -t topic -h iot-platform.cloud -p 6301 -r")
-    clean_retain_pub_cmd = shlex.split("mosquitto_pub -n  -t topic -h iot-platform.cloud -p 6301 -r")
-    sub_cmd = shlex.split("mosquitto_sub -t topic -h iot-platform.cloud -p 6301")
+    retain_pub_cmd = shlex.split("mosquitto_pub -m message  -t topic -h localhost -p 1883 -r")
+    clean_retain_pub_cmd = shlex.split("mosquitto_pub -n  -t topic -h localhost -p 1883 -r")
+    sub_cmd = shlex.split("mosquitto_sub -t topic -h localhost -p 1883")
     process = subprocess.Popen(retain_pub_cmd, 
                                stdout=subprocess.PIPE,
                                universal_newlines=True)
@@ -69,12 +70,13 @@ def test_retain():
                                stdout=subprocess.PIPE,
                                universal_newlines=True)
 
+    print("Retain test passed!")
 
 def test_v4_v5():
-    sub_cmd_v4 = shlex.split("mosquitto_sub -t topic -h iot-platform.cloud -p 6301 -V 311")
-    sub_cmd_v5 = shlex.split("mosquitto_sub -t topic -h iot-platform.cloud -p 6301 -V 5")
-    pub_cmd_v4 = shlex.split("mosquitto_pub -m message  -t topic -h iot-platform.cloud -p 6301 -V 311")
-    pub_cmd_v5 = shlex.split("mosquitto_pub -m message  -t topic -h iot-platform.cloud -p 6301 -V 5")
+    sub_cmd_v4 = shlex.split("mosquitto_sub -t topic -h localhost -p 1883 -V 311")
+    sub_cmd_v5 = shlex.split("mosquitto_sub -t topic -h localhost -p 1883 -V 5")
+    pub_cmd_v4 = shlex.split("mosquitto_pub -m message  -t topic -h localhost -p 1883 -V 311")
+    pub_cmd_v5 = shlex.split("mosquitto_pub -m message  -t topic -h localhost -p 1883 -V 5")
 
     process1 = subprocess.Popen(sub_cmd_v4,
                                stdout=subprocess.PIPE,
@@ -88,7 +90,6 @@ def test_v4_v5():
     while True:
         output = process1.stdout.readline()
         if output.strip() == 'message':
-            print("pass")
             process1.terminate()
             break
 
@@ -104,14 +105,14 @@ def test_v4_v5():
     while True:
         output = process3.stdout.readline()
         if output.strip() == 'message':
-            print("pass")
             process3.terminate()
             break
+    print("V4/V5 test passed!")
             
 
 def test_will_topic():
-    pub_cmd = shlex.split("mosquitto_pub -h iot-platform.cloud -p 6301 -t msg -d -l --will-topic will_topic --will-payload will_payload")
-    sub_cmd = shlex.split("mosquitto_sub -t will_topic -h iot-platform.cloud -p 6301")
+    pub_cmd = shlex.split("mosquitto_pub -h localhost -p 1883 -t msg -d -l --will-topic will_topic --will-payload will_payload")
+    sub_cmd = shlex.split("mosquitto_sub -t will_topic -h localhost -p 1883")
 
     process1 = subprocess.Popen(sub_cmd,
                                stdout=subprocess.PIPE,
@@ -130,6 +131,7 @@ def test_will_topic():
             process1.terminate()
             break
 
+    print("Will topic test passed!")
 
 
 
