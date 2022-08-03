a4559b1435392b8bf72a9422eb1a619c86bbaa23
lee-emqx <lihj@emqx.io>
2022-08-03 16:30:57 +0800

    * NEW [ws_v5_test] add session expiry interval.

--- ws_v5_test.py
+++ ws_v5_test.py
@@ -8,6 +8,9 @@ from paho.mqtt.properties import *
 from multiprocessing import Process, Value
 from threading import Thread, Lock
 
+import subprocess
+import shlex
+
 g_send_times = 0
 g_recv_times = 0
 g_pub_times = 1
@@ -15,6 +18,7 @@ g_sub_times = 0
 
 user_properties = [("filename","test.txt"),("count","1")]
 topic_alias = 10
+session_expiry_interval = 5
 
 def on_message_topic_alias(self, obj, msg):
     print("Receive:" + msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
@@ -32,6 +36,12 @@ def on_message_user_property(self, obj, msg):
         assert msg.properties.UserProperty == user_properties
     self.disconnect()
 
+def on_message_session_expiry_interval(self, obj, msg):
+    print("fuck")
+    print("Receive:" + msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+    assert msg.topic == str(msg.payload, 'utf-8')
+    self.disconnect()
+
 def on_message(self, obj, msg):
     print("Receive:" + msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
     assert msg.topic == str(msg.payload, 'utf-8')
@@ -40,7 +50,6 @@ def on_message(self, obj, msg):
     self.disconnect()
 
 def on_publish(self, obj, mid):
-    print("published v" + str(self._protocol))
     self.disconnect()
 
 def on_subscribe(self, obj, mid, granted_qos):
@@ -50,28 +59,50 @@ def on_subscribe(self, obj, mid, granted_qos):
     
 def on_subscribe_v5(self, mqttc, obj, mid, granted_qos):
     global g_sub_times
-    print("subscribed v5")
     # print("subscribed" + self.protocol)
     g_sub_times += 1
 
+def on_subscribe_session_expiry_interval(self, mqttc, obj, mid, granted_qos):
+    global g_sub_times
+    if g_sub_times == 0:
+        self.disconnect()
+        g_sub_times += 1
+
+def on_log(client, userdata, level, buf):
+    print("log: ",buf)
+
 def func(proto, cmd, topic, prop=None):
     mqttc = mqtt.Client(transport='websockets', protocol=proto)   
-    if "user/property" == topic:
-        mqttc.on_message = on_message_user_property
-    elif "topic/alias" == topic:
-        mqttc.on_message = on_message_topic_alias
+    mqttc.on_log = on_log
+    if cmd == "sub":
+        mqttc._client_id = "whoami/sub"
     else:
-        mqttc.on_message = on_message
-
-
-    mqttc.on_publish = on_publish
+        mqttc._client_id = "whoami/pub"
 
     if proto == MQTTv311:
         mqttc.on_subscribe = on_subscribe
     elif proto == MQTTv5:
         mqttc.on_subscribe = on_subscribe_v5
 
-    mqttc.connect("localhost", 8085, 60)
+    if "user/property" == topic:
+        mqttc.on_message = on_message_user_property
+    elif "topic/alias" == topic:
+        mqttc.on_message = on_message_topic_alias
+    elif "session/expiry/interval" == topic:
+        mqttc.on_subscribe = on_subscribe_session_expiry_interval
+        mqttc.on_message = on_message_session_expiry_interval
+    else:
+        mqttc.on_message = on_message
+
+    mqttc.on_publish = on_publish
+
+
+    if "session/expiry/interval" == topic and cmd == "sub":
+        mqttc.connect("localhost", 8085, 60, properties=prop)
+        prop = None
+    else:
+        mqttc.connect("localhost", 8085, 60)
+
 
     global g_sub_times
     if proto == MQTTv311:
@@ -84,8 +115,7 @@ def func(proto, cmd, topic, prop=None):
             mqttc.publish(topic, topic, 1)
     else:
         if cmd == "sub":
-            mqttc.subscribe(topic, 1)
-            # mqttc.subscribe("v311/to/v5", 1)
+            mqttc.subscribe(topic, 1, properties=prop)
         elif cmd == "pub":
             while g_sub_times == 0:
                 continue    
@@ -127,8 +157,62 @@ def ws_topic_alias():
     t2.start()
 
 
+def ws_session_expiry_interval():
+    properties=Properties(PacketTypes.CONNECT)
+    properties.SessionExpiryInterval=session_expiry_interval
+    # sub with property session expiry interval and disconnect when sub success
+    # pub within session expiry interval
+    # sub within session expiry interval
+    # 
+    # sub with property session expiry interval and disconnect when sub success
+    # pub within session expiry interval
+    # sub beyond session expiry interval
+
+    t1 = Thread(target=func, args=(MQTTv5, "sub", "session/expiry/interval", properties))
+    t1.start()
+    time.sleep(0.05)
+
+
+    # global g_sub_times
+    # g_sub_times = 1
+    t2 = Thread(target=func, args=(MQTTv5, "pub", "session/expiry/interval"))
+    t2.daemon = True
+    t2.start()
+    time.sleep(0.05)
+
+    # TODO
+    # It's temp test, cause I do not know why phao.mqtt interface can not receive session expiry message
+    sub_cmd = shlex.split("mosquitto_sub -t 'session/expiry/interval' -p 1883 --id whoami/sub -x 5 -c -q 1 -V 5")
+    process = subprocess.Popen(sub_cmd,
+                               stdout=subprocess.PIPE,
+                               universal_newlines=True)
+
+    output = process.stdout.readline()
+    assert output.strip() == 'session/expiry/interval'
+
+
+    # global g_sub_times
+    # g_sub_times = 1
+    # t3 = Thread(target=func, args=(MQTTv5, "sub", "session/expiry/interval"))
+    # t3.daemon = True
+    # t3.start()
+    # time.sleep(0.05)
+
+    # t4 = Thread(target=func, args=(MQTTv5, "pub", "session/expiry/interval"))
+    # t4.daemon = True
+    # t4.start()
+    # time.sleep(0.05)
+
+
+
+    # t4 = Thread(target=func, args=(MQTTv5, "sub", "session/expiry/interval", properties))
+    # t4.start()
+
+
+
+
+
 ws_v4_v5_test()
-
 ws_user_properties()
-
 ws_topic_alias()
+ws_session_expiry_interval()
